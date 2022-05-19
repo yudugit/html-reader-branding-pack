091b046fe84e6b3c7eb4b4a6443a4623dbd48143
Ruxandra Codreanu <ruxandra.codreanu@yudu.com>
2022-05-19 20:27:51 +0100

    Add vertical padding to main toolbar button icons if needed
    The padding is added such that the total height of an icon matches its max height. This is needed in order to properly vertically align all the buttons.

--- toolbar/toolbar.js
+++ toolbar/toolbar.js
@@ -208,10 +208,22 @@ var createButton = function(id, callback, highResIcons) {
     var altText = yudu_commonFunctions.getLocalisedStringByCode('toolbar.button.' + id);
     icon.attr('aria-label', altText.indexOf('toolbar.button.') === 0 ? id : altText).appendTo(button);
 
+    icon.on('load', function() {
+        setButtonIconPadding(icon);
+    });
+
     newButton(id, button);
     button.insertBefore($('#rightControls'));
 
     return button;
+};
+
+var setButtonIconPadding = function(icon) {
+    var iconHeight = parseFloat(icon.css('height'));
+    var iconMaxHeight = parseFloat(icon.css('max-height'));
+    var verticalPadding = (iconMaxHeight - iconHeight) / 2;
+    icon.css('padding-top', verticalPadding);
+    icon.css('padding-bottom', verticalPadding);
 };
 
 var createButtonNoIcon = function(id, callback) {
@@ -249,6 +261,7 @@ var showButton = function(id) {
             visibleButtons[id] = buttons[id];
             visibleButtons.__yudu_count += 1;
         }
+        setButtonIconPadding(buttons[id].find('img'));
     }
 };
 
